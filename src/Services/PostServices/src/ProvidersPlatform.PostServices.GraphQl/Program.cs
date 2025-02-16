4949cb49b3466ec33bc6c19453d090103d88dae1
Amapola <htrsanbl@gmail.com>
2025-02-16 17:26:11 -0500

    Implementation to GraphQL on http Server, successfully

--- src/Services/PostServices/src/ProvidersPlatform.PostServices.GraphQl/Program.cs
+++ src/Services/PostServices/src/ProvidersPlatform.PostServices.GraphQl/Program.cs
@@ -1,4 +1,8 @@
+using GraphQL;
+using ProvidersPlatform.PostServices.GraphQl.Graphs;
+using ProvidersPlatform.PostServices.GraphQl.Graphs.Queries;
 using ProvidersPlatform.Shared.Setup;
+using ProvidersPlatform.Shared.Setup.Configs;
 using ProvidersPlatform.Shared.Setup.Defaults;
 
 namespace ProvidersPlatform.PostServices.GraphQl;
@@ -7,8 +11,31 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var app = DefaultApiProject.GenerateWebApplicationTemplate();
-        
+        var app = DefaultApiProject.GenerateWebApplicationTemplate(opt =>
+        {
+            
+            //Injection to Providers Platform Database
+            var mariaDbConfig = new MariaDbConfig();
+            opt.Configuration.GetSection("Databases:Providers_Platform").Bind(mariaDbConfig);
+            opt.Services.AddProvidersPlatformDatabase(mariaDbConfig);
+            
+            //Injection to custom services
+            opt.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            
+            //Injection to GraphQL library
+            opt.Services.AddGraphQL(conf =>
+            {
+                conf.AddSchema<DefaultSchema>();
+                conf.AddSystemTextJson();
+            });
+            
+            //Injection to Graph Queries
+            opt.Services.AddSingleton<PostQueries>();
+            opt.Services.AddSingleton<RootQuery>();
+        });
+
+        app.UseAuthorization();
+        app.UseGraphQL<DefaultSchema>();
         app.MapGet("/healthy", () => "GraphQl Posts Api Are Healthy");
         
         DefaultApiProject.RunApplication(app);
